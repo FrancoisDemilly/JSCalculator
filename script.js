c5db5cb4d2486c876d9e2d88416d7f51aefa9e7d
FrancoisDemilly <francois.demilly@gmail.com>
2018-01-07 20:53:06 -0500

    errors fixed

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ window.onload = function(){
   var memory = "";
   var operator ="";
   var MAXLENGTH = 16;
-  var forbidden = ['+', '-', '/', '*', '.'];
+  var operatorList = ['+', '-', '/', '*', '.']; // see note function checkOperator,
 
 
 
@@ -26,6 +26,7 @@ window.onload = function(){
         addDigit();
         manageDot();
         manageOperator();
+        manageParenthesis();
         compute();
         afterComputing();
         checkOperator();
@@ -61,29 +62,49 @@ function addDigit(){
 
   function manageOperator(){
     if(keyType === "operators"){
-        if (key == "+"){
-          memory += current;
-          operator = "+";
-          current = "";
-          memory += operator;
-        }else if (key == "-"){
-          memory += current;
-          operator = "-";
-          current = "";
-          memory += operator;
-        }else if (key == "/"){
-          memory += current;
-          operator = "/";
-          current = "";
-          memory += operator;
-        }else if (key == "*"){
-          memory += current;
-          operator = "*";
-          current = "";
-          memory += operator;
-        }
-    }
+        memory += current;
+        operator = key;
+        current = "";
+        memory += operator
+
+    }//end of if operator
   }//end of manageOperator
+
+  function manageParenthesis(){
+    if(keyType === "parenthesis"){
+      var lastOfCurrent = current.charAt(current.length-1);
+      var lastOfMemory = memory.charAt(memory.length-1);
+      var currentIsOp = operatorList.includes(lastOfCurrent);
+      var memoryIsOp = operatorList.includes(lastOfMemory);
+
+
+        if (key === "("){
+          if(current === "" && memory === ""){
+            current += key;
+          }
+          else if ((currentIsOp === false || current === "") &&
+            (memoryIsOp  === true  )){
+          current += key;
+          }
+      }
+
+        if(key === ")"){
+          if (current !== "" && lastOfCurrent !== "("){
+            current += key;
+          }
+        }
+
+
+
+        //prevent enterring ) before (
+        if (current.indexOf("(") === -1 && memory.indexOf("(") === -1){
+
+          if(key === ")"){
+            current = current.slice(0,-1);
+          }
+        }
+    }//end of if Keytype
+  }//end of manageParenthesis
 
 
 // make the calculus
@@ -101,28 +122,44 @@ function addDigit(){
     if (result != "" && (keyType == 'digits' || keyType == 'dot')){
       result= result;
       current = "";
-
-
     }else if (result != "" && keyType == 'operators'){
       memory = result + key ;
       result = "";
     }
-
   }
 
 
 // preventing that 2 operators can be enter as first or
-// preventing that 2 operator can folows each other.
+// preventing that 2 operator can folows each other exept for minus
 
  function checkOperator (){
      var last = memory.charAt(memory.length-1);
      var previous = memory.charAt(memory.length-2);
-     var lastIsOperator = forbidden.includes(last);
-     var previousIsOperator = forbidden.includes(previous);
+     var third = memory.charAt(memory.length-3);
+     var lastIsOperator = operatorList.includes(last);
+     var previousIsOperator = operatorList.includes(previous);
+// preventing that 2 operators can be enter as first or
+// preventing that 2 operator can folows each other exept for minus
+      if (lastIsOperator === true   && previousIsOperator === true ){
+        memory = memory.slice(0,-1);
+        };
 
-     if (lastIsOperator === true && previousIsOperator === true){
-       memory = memory.slice(0,-1);
-     }
+
+      //preventing entering / * + after a (
+      if (previous === "(" ){
+        if(last === "/"
+        || last === "*" || last === "+"  ){
+          memory = memory.slice(0, -1);
+      }
+    }
+
+      //preventing entering / * + as at the beguinig
+      if ((last === "/"
+        || last === "*" || last === "+") && memory.length === 1 ){
+          memory = memory.slice(0, -1);
+      }
+
+
  }//end of checkOperator
 
 //clear last entry
@@ -130,8 +167,13 @@ function addDigit(){
  function clearLastEntry(){
       if(keyType === "clearLast"){
           if(current != "" ){
+          //clear the "too long at with CE click"
+            if(current.charAt(current.length-1) === "g"){
+                current = current.slice(0,-8);
+            }else{
               current = current.slice(0,-1);
               }
+            }
           else if (current === "" ){
               memory = memory.slice(0,-1);
               }
